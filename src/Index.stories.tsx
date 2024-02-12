4bdda4c802307316fdbd56a81dbe43c2237adb81
Ghassanooooo <ghassanaldarwish@yahoo.com>
2024-02-12 16:17:18 +0100

    first commit

---
+++ src/Index.stories.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Meta, StoryObj } from "@storybook/react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./index";
+
+const meta: Meta<typeof Accordion> = {
+  component: Accordion,
+  title: "Marbella/Accordion",
+  argTypes: {},
+};
+export default meta;
+
+type Story = StoryObj;
+
+export const Primary: Story = (args: any) => (
+  <Accordion type="single" collapsible>
+    <AccordionItem value="item-1">
+      <AccordionTrigger>Is it accessible?</AccordionTrigger>
+      <AccordionContent>
+        Yes. It adheres to the WAI-ARIA design pattern.
+      </AccordionContent>
+    </AccordionItem>
+    <AccordionItem value="item-2">
+      <AccordionTrigger>Is it styled?</AccordionTrigger>
+      <AccordionContent>
+        Yes. It comes with default styles that matches the other
+        components&apos; aesthetic.
+      </AccordionContent>
+    </AccordionItem>
+    <AccordionItem value="item-3">
+      <AccordionTrigger>Is it animated?</AccordionTrigger>
+      <AccordionContent>
+        Yes. It's animated by default, but you can disable it if you prefer.
+      </AccordionContent>
+    </AccordionItem>
+  </Accordion>
+);
+Primary.args = {
+  primary: true,
+  disabled: false,
+  text: "Primary",
+};
